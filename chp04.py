3c248dfad70ad47ab064e19f7400f363b9d9d74c
ning <lu>
2014-10-19 21:41:27 +0800

    ch04

--- chp04.py
+++ chp04.py
@@ -1,1 +1,28 @@
 __author__ = 'ninglu'
+from gensim import corpora, models, similarities
+import numpy as np
+corpus = corpora.BleiCorpus('data/ch04/ap/ap.dat','data/ch04/ap/vocab.txt')
+
+model = models.ldamodel.LdaModel(corpus, num_topics=100, id2word=corpus.id2word)
+
+topics = [model[c] for c in corpus]
+print(topics[0])
+
+dense = np.zeros((len(topics), 100), float)
+for ti,t in enumerate(topics):
+    for tj, v in t:
+        dense[ti,tj]= v
+
+
+from scipy.spatial import distance
+pairwise = distance.squareform(distance.pdist(dense))
+pairwise.shape
+largest = pairwise.max()
+
+for ti in range(len(topics)):
+    pairwise[ti,ti]=largest+1
+
+def closet_to(doc_id):
+    return pairwise[doc_id].argmin()
+
+print(closet_to(0))
